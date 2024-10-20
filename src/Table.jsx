e82d318d6e0f214e1376e34ae1bebf7cf0304fce
Andrey <brovkin_58@mail.ru>
2024-10-20 11:43:29 +0300

    update sort table exercise useImmer

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,49 +1,39 @@
 import { isNumber } from 'lodash';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { useImmer } from 'use-immer';
 
 export default function Table({ data }) {
   const [sortColumn, setSortColumn] = useState(Object.keys(data[0])[0]);
-  const [sortData, updateSortData] = useState(data);
+  const [sortData, updateSortData] = useImmer(data);
 
-  useEffect(() => {
-    let newData = sortData.slice();
-    if (sortColumn === 'reverse') {
-      console.log('reverse');
-      newData.reverse();
-    } else {
-      console.log('sort by ', sortColumn);
-      if (isNumber(newData[0][sortColumn]))
-        newData.sort((obj1, obj2) => obj1[sortColumn] - obj2[sortColumn]);
-      else
-        newData.sort((obj1, obj2) =>
-          obj1[sortColumn].localeCompare(obj2[sortColumn])
-        );
+  function handleSortTable(sortBy) {
+    if (sortBy === sortColumn) updateSortData((draft) => draft.reverse());
+    else {
+      updateSortData((draft) =>
+        isNumber(draft[0][sortBy])
+          ? draft.sort((obj1, obj2) => obj1[sortBy] - obj2[sortBy])
+          : draft.sort((obj1, obj2) => obj1[sortBy].localeCompare(obj2[sortBy]))
+      );
+      setSortColumn(sortBy);
     }
-    updateSortData(newData);
-  }, [sortColumn]);
+  }
 
   return (
     <table className="table">
       <Thead
         headers={Object.keys(sortData[0])}
-        sortColumn={sortColumn}
-        setSortColumn={setSortColumn}></Thead>
+        handleSortTable={handleSortTable}></Thead>
       <Tbody data={sortData}></Tbody>
     </table>
   );
 }
 
-function Thead({ headers, sortColumn, setSortColumn }) {
+function Thead({ headers, handleSortTable }) {
   return (
     <thead>
       <tr>
         {headers.map((header, i) => (
-          <th
-            key={i}
-            onClick={(e) => {
-              if (sortColumn === header) setSortColumn('reverse');
-              else setSortColumn(header);
-            }}>
+          <th key={i} onClick={(e) => handleSortTable(header)}>
             {header}
           </th>
         ))}
